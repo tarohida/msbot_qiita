acf9b38a3c980c8a83f7d5e2bcb5a3ccea327cf5
taro-hida <hidataro11@gmail.com>
2019-01-10 04:11:08 +0900

    fix

--- msbot.py
+++ msbot.py
@@ -10,11 +10,11 @@ mastodon = Mastodon(
 	access_token="/root/python/msbot_qiita/account_info/auth.txt",
 	api_base_url = "https://qiitadon.com") #インスタンス
 num = random.randrange(1, 10)
-if num <= 4:
+#if num <= 4:
 #	mastodon.toot("ﾊﾂﾈﾐｸ") #ここを変える
-elif 4 < num <= 5:
+#elif 4 < num <= 5:
 #	mastodon.toot("ﾊﾐﾂﾈｸ") #ここを変える
-elif 5 < num <= 6:
+#elif 5 < num <= 6:
 	mastodon.toot("ブチ殺すぞ…… kill you")
 #	mastodon.toot("ﾊﾂﾐﾈｸ") #ここを変える
 # mastodon.toot("ﾊ゛ﾂ゛ﾈ゛ﾐ゛ｸ゛") #ここを変える
