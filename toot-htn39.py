14ca8878f0e90f10484b9ebc79f3285a08e66ca6
taro-hida <sk8trou@gmail.com>
2021-08-28 15:33:55 +0900

    esa410 コードが読みやすくなるよう、不要なコメントの削除、インデントの修正を実施

--- toot-htn39.py
+++ toot-htn39.py
@@ -2,10 +2,9 @@ from mastodon import Mastodon
 import random
 
 mastodon = Mastodon(
-	client_id="./account_info/cred.txt",
-	access_token="./account_info/auth.txt",
-	api_base_url = "https://qiitadon.com") #インスタンス
+    client_id="./account_info/cred.txt",
+    access_token="./account_info/auth.txt",
+    api_base_url="https://qiitadon.com")
 num = random.randrange(1, 100)
 if num <= 39:
-	mastodon.toot("ﾊﾂﾈﾐｸ") #ここを変える
-
+    mastodon.toot("ﾊﾂﾈﾐｸ")
