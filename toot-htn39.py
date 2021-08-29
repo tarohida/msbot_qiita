b1fcab46d7f9dece684040fd89847dd81d9cfeaa
taro-hida <sk8trou@gmail.com>
2021-08-29 09:12:11 +0900

    esa410 toot-htn39.py に IsHatsuneBirthdayService を組み込み

--- toot-htn39.py
+++ toot-htn39.py
@@ -1,10 +1,16 @@
 from mastodon import Mastodon
+from libs.IsHatsuneBirthdayService import IsHatsuneBirthdayService
 import random
 
 mastodon = Mastodon(
     client_id="./account_info/cred.txt",
     access_token="./account_info/auth.txt",
     api_base_url="https://qiitadon.com")
+
+hatsune = IsHatsuneBirthdayService()
+if hatsune.is_birthday():
+    mastodon.toot("ﾊﾋﾟﾊﾞﾐｸ")
+
 num = random.randrange(1, 100)
 if num <= 39:
     mastodon.toot("ﾊﾂﾈﾐｸ")
