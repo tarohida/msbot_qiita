62c5a5b2f141158f8db1142ac557dde8fa5f39fb
taro-hida <sk8trou@gmail.com>
2021-08-29 09:49:39 +0900

    esa410 False を返す場合のテストケースの追加

--- libs/test_IsHatsuneBirthdayService.py
+++ libs/test_IsHatsuneBirthdayService.py
@@ -8,6 +8,18 @@ class TestIsHatsuneBirthdayService(TestCase):
         service = IsHatsuneBirthdayService()
         self.assertFalse(service.is_birthday())
 
+    def test_method_is_birthday_when_yesterday_return_False(self):
+        service = IsHatsuneBirthdayService()
+        day = datetime.date(2021, 8, 30)
+        service.set_date(day)
+        self.assertFalse(service.is_birthday())
+
+    def test_method_is_birthday_when_tomorrow_return_False(self):
+        service = IsHatsuneBirthdayService()
+        day = datetime.date(2021, 9, 1)
+        service.set_date(day)
+        self.assertFalse(service.is_birthday())
+
     def test_method_is_birthday_when_start_of_day_return_True(self):
         service = IsHatsuneBirthdayService()
         day = datetime.datetime(2021, 8, 31, hour=0, minute=0, second=0, microsecond=0)
